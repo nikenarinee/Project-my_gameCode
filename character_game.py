d24d5f928516c288884093c19b5e4e87c4f04320
KANRAYAKORN <kanrayakorn.sur@gmail.com>
2025-01-17 21:55:22 +0700

    Update character_game.py
    เพิ่มมมมม

--- character_game.py
+++ character_game.py
@@ -13,3 +13,12 @@ def update(self):
         self.rect.x -= 5
     if keys[pygame.K_RIGHT]:
         self.rect.x += 5
+    if keys[pygame.K_UP]:
+        self.rect.x -= 5
+    if keys[pygame.K_DOWN]:
+        self.rect.y += 5
+
+def shoot(self):
+    bullet = Bullet(self.rect.centerx, self.rect.top)
+    all_sprites.add(bullet)
+    bullets.add(bullet)
