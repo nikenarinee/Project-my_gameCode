a0629cedc3cb33539056d986b74d4da966c461aa
KANRAYAKORN <kanrayakorn.sur@gmail.com>
2025-01-17 21:40:59 +0700

    Update character_game.py
    แก้1

--- character_game.py
+++ character_game.py
@@ -1,4 +1,6 @@
 class Player(pagame.sprite.Sprite):
     def__init__(self):
     super().__init__()
-gghj
+    self.image = pygame.Surface((50,50))
+    self.image.fill(BLACK)
+    self.rect = self.image.get_rect()
