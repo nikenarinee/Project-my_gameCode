c072ea123e1a1cfe22845c02126dd01b537a82f5
Tatiya67-9 <mamoontatiya@gmail.com>
2025-01-26 21:38:03 +0700

    Update enemy_movement.py

--- enemy_movement.py
+++ enemy_movement.py
@@ -8,4 +8,14 @@ class Enemy(pygame.sprite.Sprite):
          self.rect.x random.randint (0, WIDTH)
          self.rect.y = random.randint(0, HEIGHT)
          self.player = player
-     def
+     def updete(self):
+        #คำนวนทิศทางไปยังผู้เล่น
+        direction_x = self.player.rect.centerx.-self.rect.centerx
+        direction_y = self.player.rect.centerx.-self.rect.centerx
+        distance = math.hypot(direction_x,direction_y)
+
+        if distance > 0:
+            direction_x /= distance
+            direction_y /= distance
+            self.rect.x += direction_x * 2 #ความเร็วการเคลื่อนที่
+            self.rect.y += direction_y * 2
